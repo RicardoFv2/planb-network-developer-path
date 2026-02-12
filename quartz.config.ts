7164faebb6091ef0ea586040bea3a3bfacd49c16
RicardoFv2 <fuentesrf0@gmail.com>
2026-02-11 23:19:26 -0600

    Configure deployment to GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ricardofv2.github.io/planb-netwokr-develope-path",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
