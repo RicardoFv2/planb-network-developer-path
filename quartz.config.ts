5b42d70352bbc84623a1b3377c8cb2cd578bbac0
RicardoFv2 <fuentesrf0@gmail.com>
2026-02-11 23:33:16 -0600

    Update baseUrl for repo rename

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ricardofv2.github.io/planb-netwokr-develope-path",
+    baseUrl: "ricardofv2.github.io/planb-netwokr-developer-path",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
