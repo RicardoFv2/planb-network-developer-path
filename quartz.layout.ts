1f8572301f25811c373f842722bd545627bc1d6a
RicardoFv2 <fuentesrf0@gmail.com>
2026-03-21 16:53:07 -0600

    refactor(layout): enable folder linking and fix header layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,10 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.MobileOnly(Component.Spacer()),
+    Component.PageTitle(),
+  ],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -39,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer({
-      folderClickBehavior: "collapse",
+      folderClickBehavior: "link",
     }),
   ],
   right: [
